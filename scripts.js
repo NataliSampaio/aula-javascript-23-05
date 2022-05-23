c131c57ebff804787a46afb3a4f7fdaec12f498a
aluno <Aluno@DEVE003018.educacional.up>
2022-05-23 09:47:55 -0300

    teoria

---
+++ scripts.js
@@ -0,0 +1,86 @@
+// function teste(){
+    // var a = 5;
+    // var b = 2;
+    //    var resultado;
+
+    // //Number para comparar com Número ou usa ""
+    // var num_dia=Number(window.prompt("Informe um número:"));
+
+    // switch(num_dia){
+    //     case 1:
+    //         resultado="Domingo";
+    //         break;
+
+    //     case 2:
+    //         resultado="Segunda";
+    //         break;
+
+    //     case 3:
+    //         resultado="Terça";
+    //         break;
+
+    //    case 4:
+    //          resultado="Quarta";
+    //          break;
+
+    //     case 5:
+    //          resultado="Quinta";
+    //          break;
+
+    //     case 6:
+    //         resultado="Sexta";
+    //         break;
+
+    //     case 7:
+    //         resultado="Sábado";
+    //         break;
+    // }
+    //coloque sempre as situações mais prováveis nos primeiros if's.
+    // if(a>b){
+    //     // document.getElementById("resultado").innerHTML="A é maior que B."
+    //    resultado= "A é maior que B."
+    //  }
+    // else{
+    //     // document.getElementById("resultado").innerHTML="B é igual ou maior que A."
+    // resultado="B é igual ou maior que A."
+
+     //} 
+
+    //dá para fazer com operador ternário,porém é bem limitado...
+    // resultado=(a>b)?"A é maior que B." : "B é maior que A" ; "B é maior ou igual a A"
+  
+    //mais prático
+    //  document.getElementById("resultado").innerHTML=resultado;
+
+
+// }
+
+
+function teste(){
+
+    var num,resultado;
+    num=1;
+    resultado=0;
+//while testa a condição no inicio e vai executar se ela for verdadeira
+    // while(num<=5){
+    //     resultado +=num;
+    //     num++;
+    // }
+    /*document.getElementById("resultado").innerHTML="O resultado é: " +resultado+ ". A variável número possui o valor " +num+ "."*/
+   //usando o Do
+//     do {
+//     resultado +=num;
+//     num++;
+//    } while(num<=5);
+   
+//usando for 
+// for(num=1;num<=5;num++){
+//     resultado+=num;
+// }
+
+
+//Outra forma de fazer  ${}
+//document.getElementById("resultado").innerHTML='O resultado é ${resultado}. A variavel num possui o valor ${num}.';
+
+}
+    
